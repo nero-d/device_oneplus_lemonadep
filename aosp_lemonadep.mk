c39435bcc6f6cf86e0d08a4124563584811e6745
nero-d <nihar.deshpande1810@gmail.com>
2022-02-17 11:17:08 +0000

    Adapt for extended.

--- aosp_lemonadep.mk
+++ aosp_lemonadep.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from lemonadep device
 $(call inherit-product, device/oneplus/lemonadep/device.mk)
 
-# Inherit some common Nameless-AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AOSP EXTENDED stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1440
